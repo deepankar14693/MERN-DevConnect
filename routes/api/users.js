9ec6560088fdb99a50375e480333e58daf81aa39
dranswal <dranswal@insightresults.com>
2019-08-20 19:25:12 +0530

    added route for registering user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load User Model
+const User = require('../../models/User')
 
 /**
  * @route GET api/users/test
@@ -7,5 +12,45 @@ const router = express.Router();
  * @access public
  */
 router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
+
+/**
+ * @route GET api/users/register
+ * @desc Register User
+ * @access public
+ */
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: 'Email already exists' });
+      }
+      else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', // Size
+          r: 'pg', // Rating
+          d: 'mm' // Default
+        });
+
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+          avatar
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+
+      }
+    })
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,12 +8,16 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
 //DB Config
 const db = require('./config/keys').mongoURI;
 
 //Connect to MongoDB
 mongoose
-  .connect(db, {useNewUrlParser: true})
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log('mongoDB connected'))
   .catch(err => console.log(err));
 
