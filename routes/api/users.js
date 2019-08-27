658cbab8232e8a2fd43872c20afa916ad90f8d36
dranswal <dranswal@insightresults.com>
2019-08-27 11:25:45 +0530

    changed response json for currentUser route

--- routes/api/users.js
+++ routes/api/users.js
@@ -102,7 +102,13 @@ router.post('/login', (req, res) => {
  */
 
 router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
-  res.json({msg: 'success' })
+  // res.json({msg: 'success' })
+  // res.json(req.user)
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  })
 })
 
 module.exports = router;
